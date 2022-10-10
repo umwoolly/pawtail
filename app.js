dc3c95bd4cec32faf30952b30f5ac895ef746485
umwoolly <umwoolly>
2022-10-10 17:25:56 -0500

    chore: configure vercel

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+// Add Express
 const express = require('express');
+const { Module } = require('module');
 const path = require('path');
+
+// Initialize Express
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, './client/public/')))
+app.use(express.static(path.join(__dirname, './client/public/')));
 
+// Create GET request
 app.get('/', (req, res)=>{
     console.log('accessing route /, METHOD = GET')
     res.sendFile(path.join(__dirname,'/client/index.html'))
-})
+});
 
+// Initialize server
 app.listen(port, ()=>{
     console.log('Listening on port: ' + port)
-})
+});
+
+// Export the Express API
+module.exports = app;
