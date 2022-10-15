6366b551e9fad3a8217126bae1149d3da883a162
umwoolly <umwoolly>
2022-10-15 11:33:17 -0500

    feat: added a login page

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ app.use(express.static(path.join(__dirname, './client/public/')));
 
 // Create GET request
 app.get('/', (req, res)=>{
-    console.log('accessing route /, METHOD = GET')
-    res.sendFile(path.join(__dirname,'/client/index.html'))
+    console.log('accessing route /, METHOD = GET');
+    res.sendFile(path.join(__dirname,'/client/index.html'));
 });
+
+app.get('/login', (req, res) => {
+    console.log('accessing route /login, METHOD = GET');
+    res.sendFile(path.join(__dirname, '/client/login.html'));
+})
 
 // Initialize server
 app.listen(port, ()=>{
-    console.log('Listening on port: ' + port)
+    console.log('Listening on port: ' + port);
 });
 
-// Export the Express API
+// Export app (Express API) so vercel can access it
 module.exports = app;
