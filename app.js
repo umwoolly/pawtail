6e1b4dfd8cfdd1786e0543fffa0918bce12f3869
umwoolly <umwoolly>
2022-10-16 15:30:50 -0500

    chores: backing up the first draft of having added signup.js for user interactions with signup form

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// middleware is a function that gets executed on every request
+// and what are we doing? we are sending the content of the public folder
 app.use(express.static(path.join(__dirname, './client/public/')));
+
+// this is being called whenever a request comes in no matter what routes the request is for
+app.use((req, res, next) => {
+    console.log('a request came in, we are in the middleware')
+    console.log(req.path)
+    // you should never do this, don't ever send a response inside the middleware because once the response is executed, it will never go to the next()
+    //res.send()
+    next()
+})
 
 // Create GET request
 app.get('/', (req, res)=>{
@@ -19,6 +30,12 @@ app.get('/login', (req, res) => {
     console.log('accessing route /login, METHOD = GET');
     res.sendFile(path.join(__dirname, '/client/login.html'));
 })
+
+app.get('/signup', (req, res) => {
+    console.log('accessing route /signup, METHOD = GET');
+    res.sendFile(path.join(__dirname, '/client/signup.html'));
+})
+
 
 // Initialize server
 app.listen(port, ()=>{
---
+++ client/public/signup.js
@@ -0,0 +1,37 @@
+console.log('inside signup.js');
+
+// what we need to do in this file are:
+// read email and read password
+const emailInput = document.getElementById('emailID')
+let email = emailInput.value;
+// emailInput.addEventListener('keyup', () => {
+//     email = emailInput.value;
+// })
+// Note to self: keyup is good for searches
+// Note to self: focusout is good for validation
+// Note to self: click is good for when you need to take an action
+
+const pswdInput = document.getElementById('pswdID')
+let password = pswdInput.value;
+// pswdInput.addEventListener('keyup', () => {
+//     password = pswdInput.value;
+// })
+
+emailInput.addEventListener('focusout', () => {
+    console.log('user is done typing');
+    email = emailInput.value;
+
+    if (email.includes('@')){
+        console.log('all\'s good');        
+    } else {
+        window.alert('please provide a valid email');
+    }
+})
+
+const signupBtn = document.getElementById('signup')
+signupBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    email = emailInput.value;
+    password = pswdInput.value;
+    console.log(`email = ${email}\npassword = ${password}`);
+})
